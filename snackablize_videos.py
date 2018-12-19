144a089cbe7e750308cf3a514c9b9a97f8307b38
HarryBrisson <ejbrisson@gmail.com>
2018-12-18 19:38:11 -0800

    function to break video into chunks

---
+++ snackablize_videos.py
@@ -0,0 +1,25 @@
+from functions.s3.s3_downloads import *
+from functions.s3.s3_uploads import *
+
+import subprocess as sp
+
+
+def snackablize_video_by_s3_key(k):
+
+    download_file_from_s3(bucket,k,'temp/temp.mpg')
+
+    filenamebase = k.split('.')[0].split('/')[-1].replace('Home Video #','').replace(' -- ','-')
+    cmd = 'ffmpeg -i temp/temp.mpg -acodec copy -f segment -segment_time 5 -vcodec copy -reset_timestamps 1 -map 0 temp/clips/{}-%04d.mp4'.format(filenamebase)
+    sp.call(cmd, shell=True)
+
+    clips = [f for f in os.listdir("temp/clips") if '.mp4' in f]
+
+    for c in clips:
+        print(c)
+        source_path = "temp/clips/{}".format(c)
+        destination_path = 'clips/{}/{}'.format(c.split('-')[0],c)
+        add_file_to_s3(source_path, 'brissonstagram', destination_path)
+        os.remove(source_path)
+
+    os.remove('temp/temp.mpg')
+    

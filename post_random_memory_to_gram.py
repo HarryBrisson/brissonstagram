839c90454ea61a0d9256ef1908a9f2cdf1999385
HarryBrisson <ejbrisson@gmail.com>
2018-12-20 21:19:27 -0800

    add function to get random frame from clip

--- post_random_memory_to_gram.py
+++ post_random_memory_to_gram.py
@@ -14,3 +14,7 @@ def download_random_clip():
     key = get_random_clip_key()
     download_file_from_s3('brissonstagram',key,'temp/tempvid.mp4')
 
+def get_random_frame_from_clip(video_filename, image_filename):
+    sec = random.randrange(5)
+    sp.call('ffmpeg -ss 00:00:0{} -i {} -y -vframes 1 -q:v 2 {}'.format(sec,video_filename,image_filename),shell=True)
+

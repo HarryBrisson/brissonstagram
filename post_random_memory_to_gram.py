eda3c1b2c263591583459943993548fc3f510a27
HarryBrisson <ejbrisson@gmail.com>
2018-12-20 21:19:11 -0800

    add function to download random clip

--- post_random_memory_to_gram.py
+++ post_random_memory_to_gram.py
@@ -10,3 +10,7 @@ def get_random_clip_key():
     key = random.choice(keys)
     return key
 
+def download_random_clip():
+    key = get_random_clip_key()
+    download_file_from_s3('brissonstagram',key,'temp/tempvid.mp4')
+

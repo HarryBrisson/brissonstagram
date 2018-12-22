061cd0d75e829095ec0c16cf85c3239e00322315
HarryBrisson <ejbrisson@gmail.com>
2018-12-21 19:53:28 -0800

    add boomerang gif creator function

--- post_random_memory_to_gram.py
+++ post_random_memory_to_gram.py
@@ -18,3 +18,15 @@ def get_random_frame_from_clip(video_filename, image_filename):
     sec = random.randrange(5)
     sp.call('ffmpeg -ss 00:00:0{} -i {} -y -vframes 1 -q:v 2 {}'.format(sec,video_filename,image_filename),shell=True)
 
+
+def create_boomerang_gif():
+
+    vid_filename = "temp/tempvid.mp4"
+    compressed_filename = "temp/tempsmall.mp4"
+    gif_filename = "temp/tempgif.gif"
+
+    small_cmd = 'ffmpeg -y -i {} -b 200000 {}'.format(vid_filename, compressed_filename)
+    gif_maker_cmd = 'ffmpeg -y -i {} -filter_complex "[0]reverse[r];[0][r]concat,loop=1:0,setpts=N/25/TB" -f gif -pix_fmt yuv420p {}'.format(compressed_filename, gif_filename)
+    sp.call(small_cmd,shell=True)
+    sp.call(gif_maker_cmd,shell=True)
+

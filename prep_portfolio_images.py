f7aa39d00d1cb4508f218f4d01b3439c1acdae30
HarryBrisson <ejbrisson@gmail.com>
2020-01-10 23:33:45 -0500

    add function to create square compressed jpg

--- prep_portfolio_images.py
+++ prep_portfolio_images.py
@@ -24,3 +24,20 @@ def get_random_frame_from_clip(video_filename, image_filename):
     sec = random.randrange(5)
     sp.call('ffmpeg -ss 00:00:0{} -i {} -y -vframes 1 -q:v 2 {}'.format(sec,video_filename,image_filename),shell=True)
 
+def create_gram_ready_image():
+
+    print('creating gram-friendly square video')
+    
+    vid_filename = "temp/rawvid.mp4"
+    square_vid_filename = "temp/square.mp4"
+    square_image_filename = "temp/square_raw.mp4"
+    compressed_filename = "temp/square.jpg"
+
+    square_cmd = 'ffmpeg -y -i {} -vf "crop=\'min(iw,1*ih)\':\'min(iw/1,ih)\',scale=720:720" {}'.format(vid_filename, square_vid_filename)
+    compress_cmd = 'ffmpeg -y -i {} -compression_level 50 {}'.format(square_image_filename, compressed_filename)
+    
+    sp.call(square_cmd,shell=True)
+    get_random_frame_from_clip(square_vid_filename, square_image_filename)
+    sp.call(compress_cmd,shell=True)
+
+

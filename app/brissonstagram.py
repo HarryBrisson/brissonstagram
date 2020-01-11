b23ed2929f55ffa48ecf3bbfedad6f4390117542
HarryBrisson <ejbrisson@gmail.com>
2020-01-10 22:50:46 -0500

    add function to sample images

--- app/brissonstagram.py
+++ app/brissonstagram.py
@@ -10,3 +10,10 @@ def get_image_data():
         images = json.loads(f.read())
 
     return images
+
+
+def get_sample_of_images(n=100):
+    images = get_image_data()
+    sample = random.sample(images,n)
+
+    

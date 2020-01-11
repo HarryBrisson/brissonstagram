fbbc7b4041d7d78915c20723f4278baec44e6618
HarryBrisson <ejbrisson@gmail.com>
2020-01-11 17:16:13 -0500

    add a function to create an image inventory

--- app/brissonstagram.py
+++ app/brissonstagram.py
@@ -14,6 +14,17 @@ def get_image_data():
 
 	return images
 
+def create_image_inventory():
+	fs = s3fs.S3FileSystem()
+	keys = fs.find('brissonstagram/jpgs')
+	
+	data = [{'url':f'https://brissonstagram.s3.amazonaws.com/{"/".join(k.split("/")[1:])}'} for k in keys]
+
+	with fs.open('brissonstagram/inventory/images.json','w') as f:
+		f.write(json.dumps(data))
+
+	return data
+
 
 def get_sample_of_images(n=100):
     images = get_image_data()

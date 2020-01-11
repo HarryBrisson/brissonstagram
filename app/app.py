219f5d8a690db98bf4d23bd370a0b3c6ea7eb1f5
HarryBrisson <ejbrisson@gmail.com>
2020-01-11 17:19:24 -0500

    run new functions to produce brissonstagram gallery

--- app/app.py
+++ app/app.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing():
-	return render_template('index.html')
+	data_string = get_sample_of_images()
+	html = render_template('index.html',
+		data_string = data_string)
+	return html
 
 		
 if __name__ == '__main__':

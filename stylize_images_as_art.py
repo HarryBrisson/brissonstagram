af99b8dc141c81fcbefca5466f3ec3ad839073b6
HarryBrisson <ejbrisson@gmail.com>
2020-11-29 19:47:16 -0600

    add script to start on style transfer for dad's bday gift

---
+++ stylize_images_as_art.py
@@ -0,0 +1,6 @@
+
+# use a style imitator to create art imitations and frame them?
+# examples:
+#  * https://towardsdatascience.com/art-style-transfer-using-neural-networks-a28f5888746b
+#  * https://medium.com/mlreview/making-ai-art-with-style-transfer-using-keras-8bb5fa44b216
+#  * https://github.com/CompVis/adaptive-style-transfer/blob/master/README.md

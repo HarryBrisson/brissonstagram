ff3c560105e720797c23a28bf5991ede80ce38f6
HarryBrisson <ejbrisson@gmail.com>
2020-12-07 19:10:22 -0600

    remove previous references

--- stylize_images_as_art.py
+++ stylize_images_as_art.py
@@ -1,9 +1,6 @@
 
 # use a style imitator to create art imitations and frame them?
 # examples:
-#  * https://towardsdatascience.com/art-style-transfer-using-neural-networks-a28f5888746b
-#  * https://medium.com/mlreview/making-ai-art-with-style-transfer-using-keras-8bb5fa44b216
-#  * https://github.com/CompVis/adaptive-style-transfer/blob/master/README.md
 
 # code from here:  https://github.com/walid0925/AI_Artistry/blob/master/main.py
 # reference paintings from: https://www.rawpixel.com/board/537381/vincent-van-gogh-free-original-public-domain-paintings

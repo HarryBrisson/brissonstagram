08d82a1244972aa0c1f9d31045773cb9c98421fe
HarryBrisson <ejbrisson@gmail.com>
2020-12-13 20:19:01 -0600

    add sudo to pip install

--- initialize.sh
+++ initialize.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get -y install ffmpeg
 
 sudo apt-get -y install python3-pip
-python3 -m pip install -r requirements.txt
+sudo python3 -m pip install -r requirements.txt

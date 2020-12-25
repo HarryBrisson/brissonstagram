77134eb04f9019ccd5576ec33a97da75aa639b8b
HarryBrisson <ejbrisson@gmail.com>
2020-12-25 05:12:14 -0600

    allow functions to pull from other folders

--- app/brissonstagram.py
+++ app/brissonstagram.py
@@ -3,33 +3,33 @@ import random
 
 import s3fs
 
-def get_image_data():
+def get_image_data(folder="jpgs"):
 	fs = s3fs.S3FileSystem()
 
 	try:
-		with fs.open('brissonstagram/inventory/images.json','r') as f:
+		with fs.open(f'brissonstagram/inventory/{folder}.json','r') as f:
 			images = json.loads(f.read())
 	except:
-		images = create_image_inventory()
+		images = create_image_inventory(folder=folder)
 
 	return images
 
-def create_image_inventory():
+def create_image_inventory(folder="jpgs"):
 	fs = s3fs.S3FileSystem()
-	keys = fs.find('brissonstagram/jpgs')
+	keys = fs.find(f'brissonstagram/{folder}')
 	
 	data = [{'url':f'https://brissonstagram.s3.amazonaws.com/{"/".join(k.split("/")[1:])}'} for k in keys]
 
-	with fs.open('brissonstagram/inventory/images.json','w') as f:
+	with fs.open(f'brissonstagram/inventory/{folder}.json','w') as f:
 		f.write(json.dumps(data))
 
 	return data
 
 
-def get_sample_of_images(n=100,force_update=False):
+def get_sample_of_images(folder="jpgs",n=100,force_update=False):
 	if force_update:
-		create_image_inventory()
-	images = get_image_data()
+		create_image_inventory(folder=folder)
+	images = get_image_data(folder=folder)
 	sample = random.sample(images,n)
 	return sample
 

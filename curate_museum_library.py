35b2936ab0c6d40d7fbe508519e1579a66b5f383
HarryBrisson <ejbrisson@gmail.com>
2020-12-25 05:11:59 -0600

    add color complexity filter

--- curate_museum_library.py
+++ curate_museum_library.py
@@ -1,8 +1,20 @@
 import s3fs
-
+from PIL import Image
 
 from stylize_images_as_art import *
 from post_random_memory import *
+
+
+def check_for_palette_complexity(image_path):
+
+	img = Image.open(image_path)
+
+	color_frequency = img.histogram()
+
+	colors_represented = sum([f > 0 for f in color_frequency])
+
+	return colors_represented
+
 
 
 def add_art_to_library(fs=None):
@@ -15,25 +27,29 @@ def add_art_to_library(fs=None):
 
 	create_gram_ready_image()
 
-	# check to see if image is just blue?
-	
-	data = style_image_with_tensorflow_hub()
+	colors_included = check_for_palette_complexity('temp/square.png')
+	print(colors_included)
 
-	style_path = data['style']
-	style = style_path.split('/')[-1].split('.')[0]
+	if colors_included > 50:
 
-	print(style)
+		data = style_image_with_tensorflow_hub()
 
-	filepath = f'brissonstagram/art/{clip_name}--{style}.jpg'
+		style_path = data['style']
+		style = style_path.split('/')[-1].split('.')[0]
 
-	with open('temp/artsy.jpg','rb') as f:
-		img_data = f.read()
-	
-	with fs.open(filepath, 'wb') as f:
-		f.write(img_data)
+		print(style)
+
+		filepath = f'brissonstagram/art/{clip_name}--{style}.jpg'
+
+		with open('temp/artsy.jpg','rb') as f:
+			img_data = f.read()
+		
+		with fs.open(filepath, 'wb') as f:
+			f.write(img_data)
 
 	clear_temp_folder_of_media_files()
 
 
 if __name__ == "__main__":
-	add_art_to_library()
+	for i in range(100):
+		add_art_to_library()

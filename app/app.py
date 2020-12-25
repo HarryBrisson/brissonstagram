a32fe01d9fe163cf2a1b105d13395c6c2eda9f27
HarryBrisson <ejbrisson@gmail.com>
2020-12-25 08:31:10 -0600

    add endpoint for vr gallery

--- app/app.py
+++ app/app.py
@@ -14,6 +14,16 @@ def landing():
 		data_string = data_string)
 	return html
 
+@app.route('/vr')
+def vr():
+	style = request.args.get('style')
+	if not style or style=="None":
+		style = "jpgs"
+	data_string = get_sample_of_images(folder=style)
+	print(data_string)
+	html = render_template('museum.html',
+		data_string = data_string)
+	return html	
 		
 if __name__ == '__main__':
     app.run(debug=True)

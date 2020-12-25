ffb1d6f02ae1a5049f6ad43880866b654739a9e3
HarryBrisson <ejbrisson@gmail.com>
2020-12-25 08:31:42 -0600

    add dimensions and ids to images

--- app/brissonstagram.py
+++ app/brissonstagram.py
@@ -31,6 +31,13 @@ def get_sample_of_images(folder="jpgs",n=100,force_update=False):
 		create_image_inventory(folder=folder)
 	images = get_image_data(folder=folder)
 	sample = random.sample(images,n)
+	for img in sample:
+		img['id'] = img['url'].split('/')[-1].split('.')[0]
+		axes = ['x','y','z']
+		random.shuffle(axes)
+		img[axes[0]] = (1+random.random()*.5)*random.choice([-1,1])
+		img[axes[1]] = 1.5-3*random.random()
+		img[axes[2]] = 1.5-3*random.random()
 	return sample
 
 	

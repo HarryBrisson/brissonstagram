e9d9149b133e75888832205de20974f373cf1cde
Harry Brisson <ejbrisson@gmail.com>
2021-12-25 04:46:46 -0600

    incorporate filtering mechanism

--- app/app.py
+++ app/app.py
@@ -10,9 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def landing():
 	style = request.args.get('style')
+	filters = request.args.get('filters')
 	if not style or style=="None":
 		style = "jpgs"
-	data_string = get_sample_of_images(folder=style)
+	if filters=='None':
+		filters=None
+	data_string = get_sample_of_images(folder=style, filters=filters)
 	html = render_template('index.html',
 		data_string = data_string)
 	return html
--- app/brissonstagram.py
+++ app/brissonstagram.py
@@ -3,16 +3,42 @@ import random
 
 import s3fs
 
-def get_image_data(folder="jpgs"):
+def get_image_data(folder="jpgs",filters=None):
 	fs = s3fs.S3FileSystem()
 
-	try:
-		with fs.open(f'brissonstagram/inventory/{folder}.json','r') as f:
+	if filters:
+		image_data_path = f'brissonstagram/inventory/{folder}-metadata.json'
+	else:
+		image_data_path = f'brissonstagram/inventory/{folder}.json'
+
+	if fs.exists(image_data_path):
+		with fs.open(image_data_path,'r') as f:
 			images = json.loads(f.read())
-	except:
+	else:
+		if filters:
+			folder = 'metadata/jpgs'
+		else:
+			folder = 'jpgs'
 		images = create_image_inventory(folder=folder)
 
+	if filters:
+
+		images = [i for i in images if i.get('palette').get('complexity')>100]
+
+		if 'red' in filters:
+			images = [i for i in images if (i.get('palette').get('R')>i.get('palette').get('B')) and (i.get('palette').get('R')>i.get('palette').get('G'))]
+
+		if 'green' in filters:
+			images = [i for i in images if (i.get('palette').get('G')>i.get('palette').get('B')) and (i.get('palette').get('G')>i.get('palette').get('R'))]
+
+		if 'blue' in filters:
+			images = [i for i in images if (i.get('palette').get('B')>i.get('palette').get('R')) and (i.get('palette').get('B')>i.get('palette').get('G'))]
+
+		if 'faces' in filters:
+			images = [i for i in images if len(i.get('faces').get('FaceDetails'))>0]
+
 	return images
+
 
 def create_image_inventory(folder="jpgs"):
 	fs = s3fs.S3FileSystem()
@@ -26,11 +52,11 @@ def create_image_inventory(folder="jpgs"):
 	return data
 
 
-def get_sample_of_images(folder="jpgs",n=100,force_update=False):
+def get_sample_of_images(folder="jpgs",n=100,force_update=False,filters=None):
 	if force_update:
 		create_image_inventory(folder=folder)
-	images = get_image_data(folder=folder)
-	sample = random.sample(images,n)
+	images = get_image_data(folder=folder,filters=filters)
+	sample = random.choices(images,k=n)
 	for img in sample:
 		img['id'] = img['url'].split('/')[-1].split('.')[0]
 		axes = ['x','y','z']
